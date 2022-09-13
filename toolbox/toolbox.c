e1c9baeb7106594fc08b2085c3ea0bd34a2433ec
Vladimir Mamykin <vlad@MacBook-Air-Vladimir.local>
2022-09-13 17:39:35 +0300

    update transitions array definition

--- toolbox/toolbox.c
+++ toolbox/toolbox.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-#define FROM	0
-#define TO	1
-#define FLAGS	2
-#define T	3
-
 #define TF_PENDING	1
 #define TF_JUMPED	2
 
-static int __transitions[TRANSITIONS+1][4];
+typedef struct __attribute__((packed)) {
+	void * from;
+	void * to;
+	unsigned int flags;
+	size_t t;
+} transition_t;
+
+static transition_t  __transitions[TRANSITIONS+1];
 
 const char*fmt = "%d::%d:%d;[%d]\n";
 
@@ -20,39 +22,39 @@ __koi_covdump(int from, int to, int id)
 
 #if DEBUG
 	printf("\t");printf("%d::%d:%d [%08x::%08x];\n", id, from, to,
-		__transitions[prev_id][FLAGS], __transitions[id][FLAGS]);
+		__transitions[prev_id].flags, __transitions[id].flags);
 #endif
 
 	if (prev_from == from){
 		// We are in a skip mark, so it was not a jump
-		if (__transitions[id - 1][FLAGS] & TF_PENDING) {
-			__transitions[id - 1][FROM] = 0;
-			__transitions[id - 1][TO] = 0;
-			__transitions[id - 1][FLAGS] = 0;
-			__transitions[id - 1][T] = 0;
+		if (__transitions[id - 1].flags & TF_PENDING) {
+			__transitions[id - 1].from = 0;
+			__transitions[id - 1].to = 0;
+			__transitions[id - 1].flags = 0;
+			__transitions[id - 1].t = 0;
 		}
-		if (__transitions[id][FLAGS] & TF_JUMPED) {
+		if (__transitions[id].flags & TF_JUMPED) {
 			// Decrement Jump temperature
-			if (__transitions[id - 1][T] > 0) {
-				__transitions[id - 1][T] -= 1;
+			if (__transitions[id - 1].t > 0) {
+				__transitions[id - 1].t -= 1;
 			}
 		}
 	} else {
 		// This was a jump, remove pending flag
 		if (prev_id != -1) {
-			__transitions[prev_id][FLAGS] &= ~TF_PENDING;
+			__transitions[prev_id].flags &= ~TF_PENDING;
 		}
 	}
 
-	__transitions[id][FROM] = from;
-	__transitions[id][TO] = to;
-	__transitions[id][T] += 1;
+	__transitions[id].from = from;
+	__transitions[id].to = to;
+	__transitions[id].t += 1;
 	
 	// It has a destination, so mark it a jump
 	if (to > 0) {
 		// If not already jumped
-		if (!(__transitions[id][FLAGS] & TF_JUMPED)) {
-			__transitions[id][FLAGS] = TF_PENDING | TF_JUMPED;
+		if (!(__transitions[id].flags & TF_JUMPED)) {
+			__transitions[id].flags = TF_PENDING | TF_JUMPED;
 		}
 	}
 	prev_from = from;
@@ -64,8 +66,10 @@ render_dump()
 {
 	int i;
 	for (i = 0; i < TRANSITIONS; i++) {
-		if (__transitions[i][FROM] || __transitions[i][TO]) {
-			printf(fmt, i, __transitions[i][FROM], __transitions[i][TO], __transitions[i][T]);
+		if (__transitions[i].from || __transitions[i].to) {
+			printf(fmt, i, __transitions[i].from,
+				__transitions[i].to, __transitions[i].t);
 		}
 	}
 }
+
